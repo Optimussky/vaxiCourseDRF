f9bcd3bd822ed193a5db1483ca4f5a748851aea5
Alberto Romero <optimussky05@gmail.com>
2022-07-13 19:44:24 -0500

    Model created and add config information

---
+++ inmuebles/inmuebleslist_app/models.py
@@ -0,0 +1,16 @@
+from statistics import mode
+from django.db import models
+from django.forms import CharField
+
+# Create your models here.
+
+
+class Inmueble(models.Model):
+    direccion = models.CharField(max_length=250)
+    pais = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=500)
+    imagen = models.CharField(max_length=900)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.direccion
